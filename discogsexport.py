fd0a1d54a104c9ada4765f97d959c756ea29ddfd
Mark IJzerman <markijzerman@gmail.com>
2019-01-08 17:46:41 +0100

    writing to file

--- discogsexport.py
+++ discogsexport.py
@@ -1,6 +1,10 @@
 import discogs_client
 import time
 
+outFile = "GONZOCIRCUS.txt"
+
+f = open(outFile, "w")
+
 d = discogs_client.Client('mind-the-gap-exporter/0.1', user_token="")
 
 # find the Gonzo label and it's releases
@@ -11,7 +15,9 @@ gonzoreleases = gonzolabel.releases
 # go over every release
 for release in gonzoreleases:
     time.sleep(0.5)
-    print(release.title)
+    print(release.title.upper())
+    f.write(release.title.upper())
+    f.write("\n")
     print( )
     for track in release.tracklist:
         artistname = ""
@@ -20,7 +26,12 @@ for release in gonzoreleases:
                 artistname += ", "
             artistname += artist.name
         title = track.title
-        print(artistname, "-", title)
-    print( )
-    print( )
+        if title != "Track 13":
+            print(artistname, "-", title)
+            f.write(artistname)
+            f.write(" - ")
+            f.write(title)
+            f.write("\n")
+    f.write("\n")
+    f.write("\n")
 
